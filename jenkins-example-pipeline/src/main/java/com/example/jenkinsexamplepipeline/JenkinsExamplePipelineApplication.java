ce8cf735d554aec9a0d26241fab782b0bfef81ac
83Incs_11 <gaurav.kumar@83incs.com>
2019-12-31 01:30:45 +0530

    added pipeline

---
+++ jenkins-example-pipeline/src/main/java/com/example/jenkinsexamplepipeline/JenkinsExamplePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexamplepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExamplePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExamplePipelineApplication.class, args);
+	}
+
+}
---
+++ jenkins-example-pipeline/src/test/java/com/example/jenkinsexamplepipeline/JenkinsExamplePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexamplepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExamplePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
